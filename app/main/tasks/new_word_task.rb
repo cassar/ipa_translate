b5f9083155afa1303d3f654d6aa2a2b097931b7b
Luke Cassar <lukecassar7@gmail.com>
2015-12-22 21:34:28 +0000

    add wiktionary retrieve

--- app/main/tasks/new_word_task.rb
+++ app/main/tasks/new_word_task.rb
@@ -1,8 +1,43 @@
 class NewWordTask < Volt::Task
+  require 'httparty'
+
   def process_words(sentence)
     sentence.downcase.split.each do |entry|
       next if store.words.where(entry: entry).first.sync.present?
-      store.words.create(entry: entry, ipa: '[new]').sync
+      create_word(entry)
     end
+  end
+
+  def create_word(entry)
+    store.words.create(
+      entry: entry,
+      ipa: return_ipa_word(entry)
+    ).sync
+  end
+
+  def return_ipa_word(entry)
+    @ipa_entry = retrieve_ipa_word_from_wiktionary(entry)
+    if @ipa_entry.present?
+      @ipa_entry
+    else
+      '[new]'
+    end
+  end
+
+  def retrieve_ipa_word_from_wiktionary(entry)
+    @word_array = []
+    @uri = URI.encode("https://en.wiktionary.org/w/index.php?title=#{entry}&printable=yes".strip)
+    @response = HTTParty.get(@uri)
+    @slash_scan = @response.scan(%r{(?<=>/)(.*?)(?=/)})
+    @word_array << @slash_scan if @slash_scan.present?
+    first_array_entry(@word_array)
+  end
+
+  def first_array_entry(word_array)
+    @numberless_array = []
+    word_array.flatten.each do |element|
+      @numberless_array << element if (/[0-9]/ =~ element).nil?
+    end
+    @numberless_array.first.gsub(%r{(\/|\s)}, '') if word_array.present?
   end
 end
--- spec/app/main/tasks/new_word_task_spec.rb
+++ spec/app/main/tasks/new_word_task_spec.rb
@@ -1,9 +1,41 @@
 require 'spec_helper'
 
 describe NewWordTask, type: :task do
-  it 'should create a new word when passed an entry' do
+  it 'should create new words when passed a sentence string' do
     NewWordTask.process_words('hello my name is')
 
     expect(store.words.count.sync).to eq(4)
+  end
+
+  it 'should create a new word' do
+    @word = NewWordTask.create_word('hello').sync
+
+    expect(store.words.count.sync).to eq(1)
+    expect(@word.entry).to eq('hello')
+    expect(@word.ipa).to eq('həˈləʊ̯')
+  end
+
+  it 'should return an ipa word' do
+    @ipa_word = NewWordTask.return_ipa_word('hello').sync
+
+    expect(@ipa_word).to eq('həˈləʊ̯')
+  end
+
+  it 'should return a placeholder' do
+    @placeholder = NewWordTask.return_ipa_word('woodchuck').sync
+
+    expect(@placeholder).to eq('[new]')
+  end
+
+  it 'should retrieve_ipa_word_from_wiktionary' do
+    @ipa_word = NewWordTask.retrieve_ipa_word_from_wiktionary('hello').sync
+
+    expect(@ipa_word).to eq('həˈləʊ̯')
+  end
+
+  it 'should return the first numberless array entry' do
+    @entry = NewWordTask.first_array_entry(%w(Hello5 http9 hello)).sync
+
+    expect(@entry).to eq('hello')
   end
 end
